fd7e058d5eea32322de746632663f6e0a82192be
apsc <78262269+aditya3284@users.noreply>
2024-09-12 17:00:02 +0530

    Handle Nav Menu State

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import NavItems from "../components/NavItems";
+
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleMenuToggle = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+
 	return (
 		<header className='fixed inset-x-0 top-0 z-10 bg-black/90'>
 			<div className='mx-auto max-w-7xl'>
@@ -12,16 +19,20 @@ const Navbar = () => {
 						Pokemon
 					</a>
 					<button
+						onClick={handleMenuToggle}
 						className='block text-neutral-400 hover:text-white focus:outline-none sm:hidden'
 						aria-label='Toggle Menu'
 					>
 						<img
-							src={"/assets/menu.svg"}
+							src={isOpen ? "/assets/close.svg" : "/assets/menu.svg"}
 							width={24}
 							height={24}
 							alt='toggle menu'
 						/>
 					</button>
+					<nav className='hidden sm:block'>
+						<NavItems />
+					</nav>
 				</div>
 			</div>
 		</header>
