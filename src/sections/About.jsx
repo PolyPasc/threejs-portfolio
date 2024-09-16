8df6a13b3a0b8940c3d4ac2b889c8387459ba3d5
apsc <78262269+aditya3284@users.noreply>
2024-09-16 10:16:28 +0530

    Add Globe to About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,6 @@
+import Globe from "react-globe.gl";
+import Button from "../components/ui/Button";
+
 const About = () => {
 	return (
 		<section aria-label='About' className='c-space my-20'>
@@ -35,7 +38,33 @@ const About = () => {
 					</div>
 				</div>
 				<div className='col-span-1 xl:row-span-4'>
-					<div className='grid-container'></div>
+					<div className='grid-container'>
+						<div className='flex h-fit w-full items-center justify-center rounded-3xl sm:h-[326px]'>
+							<Globe
+								height={326}
+								width={326}
+								backgroundColor='rgba(0,0,0,0)'
+								showAtmosphere
+								showGraticules
+								globeImageUrl='//unpkg.com/three-globe/example/img/earth-night.jpg'
+							/>
+						</div>
+						<div className=''>
+							<p className='grid-headText'>
+								I work remotely across most timezones.
+							</p>
+							<p className='grid-subText'>
+								I'm based in Eygpt, with remote work available.
+							</p>
+							<a href='#contact'>
+								<Button
+									text='Contact Me'
+									isBeam
+									containerClass={"mt-10 w-full"}
+								/>
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
