0ae583623e5c7373d2bd573e8f8e34ea59a68aee
apsc <78262269+aditya3284@users.noreply>
2024-09-16 13:11:50 +0530

    Create Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./sections/About";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
 
 function App() {
 	return (
 		<main className='mx-auto max-w-7xl'>
 			<Navbar />
 			<Hero />
-			<About/>
+			<About />
+			<Projects />
 		</main>
 	);
 }
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,67 @@
+import { myProjects } from "../constants";
+
+const Projects = () => {
+	const currentProject = myProjects[0];
+
+	return (
+		<section aria-label='Projects' className='c-space my-20'>
+			<h2 className='head-text'>My Work</h2>
+			<div className='mt-12 grid w-full grid-cols-1 gap-5 lg:grid-cols-2'>
+				<div className='relative flex flex-col gap-5 px-5 py-10 shadow-2xl shadow-black-200 sm:p-10'>
+					<div className='absolute right-0 top-0'>
+						<img
+							src={currentProject.spotlight}
+							alt=''
+							className='h-96 w-full rounded-xl object-cover'
+						/>
+					</div>
+					<div
+						className='w-fit rounded-lg p-3 backdrop-blur-3xl backdrop-filter'
+						style={currentProject.logoStyle}
+					>
+						<img src={currentProject.logo} className='size-10 shadow-sm' />
+					</div>
+					<div className='my-5 flex flex-col gap-5 text-white-600'>
+						<h3 className='text-2xl font-semibold text-white'>
+							{currentProject.title}
+						</h3>
+						<p>{currentProject.desc}</p>
+						<p>{currentProject.subdesc}</p>
+					</div>
+					<div className='flex flex-wrap items-center justify-between gap-5'>
+						<div className='flex items-center gap-3'>
+							{currentProject.tags.map((tag, index) => (
+								<div key={index} className='tech-logo'>
+									<img src={tag.path} alt={tag.name} title={tag.name} />
+								</div>
+							))}
+						</div>
+						<a
+							href={currentProject.href}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='flex cursor-pointer items-center gap-2 text-white-600'
+						>
+							<p>Check Live Site</p>
+							<img src='/assets/arrow-up.png' className='size-3' alt='arrow' />
+						</a>
+					</div>
+					<div className='mt-7 flex items-center justify-between'>
+						<button className='arrow-btn'>
+							<img src='/assets/left-arrow.png' className='size-4' alt='left' />
+						</button>
+						<button className='arrow-btn'>
+							<img
+								src='/assets/right-arrow.png'
+								className='size-4'
+								alt='right'
+							/>
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Projects;
