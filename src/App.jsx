724cfac8c689199d4d967c72e0e5a31fcded57e5
apsc <78262269+aditya3284@users.noreply>
2024-09-17 12:38:15 +0530

    Create Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./sections/About";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -16,6 +17,7 @@ function App() {
 			<Testimonials />
 			<WorkExperience />
 			<Contact />
+			<Footer />
 		</main>
 	);
 }
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,47 @@
+import { socials } from "../constants";
+
+const Footer = () => {
+	return (
+		<footer id='footer' className='c-space border-t border-black-300 pb-3 pt-7'>
+			<div className='flex flex-wrap items-center justify-between gap-5'>
+				<div className='mx-auto flex gap-2 text-white-500'>
+					<a className='cursor-pointer' href=''>
+						Terms & Conditions
+					</a>
+					|
+					<a className='cursor-pointer' href=''>
+						Privacy Policy
+					</a>
+				</div>
+				<div className='mx-auto flex gap-3'>
+					{socials.map(({ id, href, name, src }) => (
+						<a
+							href={href}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='social-icon'
+						>
+							<img src={src} alt={name} title={name} className='size-1/2' />
+						</a>
+					))}
+				</div>
+				<p className='mx-auto text-white-500'>
+					&copy; {new Date().getUTCFullYear()} Pokemon. All rights reserved
+				</p>
+			</div>
+			<p className='my-10 text-center text-white-500'>
+				Made with 💖 by
+				<a
+					href='https://'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='cursor-pointer text-white-600'
+				>
+					Pokemon
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
