68ef4809fe889d8577706c0b3df6660e17a3be62
apsc <78262269+aditya3284@users.noreply>
2024-09-19 21:18:51 +0530

    Modify constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,11 +40,11 @@ export const calculateSizes = (isSmall, isMobile) => {
 
 export const myProjects = [
 	{
-		title: "Podmastr -  Podcast Platform",
-		desc: "Podmastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced features, it allows creators to generate diverse type of content.",
+		title: "Podmastr 3000 -  Podcast Platform",
+		desc: "Podmastr 3000 is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced features, it allows creators to generate diverse type of content.",
 		subdesc:
-			"Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podmastr is designed for optimal performance and scalability.",
-		href: "https://www.youtube.com/watch?v=zfAb95tJvZQ",
+			"Built as a unique Software-as-a-Service app with React.js, Tailwind CSS and Framer Motion, Podmastr 3000 is designed for optimal performance and scalability.",
+		href: "https://www.github.com/",
 		texture: "/textures/project/project1.mp4",
 		logo: "/assets/project-logo1.png",
 		logoStyle: {
@@ -66,22 +66,17 @@ export const myProjects = [
 			},
 			{
 				id: 3,
-				name: "TypeScript",
-				path: "/assets/typescript.png",
-			},
-			{
-				id: 4,
 				name: "Framer Motion",
 				path: "/assets/framer.png",
 			},
 		],
 	},
 	{
-		title: "LiveDoc - Real-Time Online Docs",
-		desc: "LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing.It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
+		title: "UrDoc - Real-Time Online Docs",
+		desc: "UrDoc is a powerful collaborative app that elevates the capabilities of real-time document editing.It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
 		subdesc:
-			"With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.",
-		href: "https://www.youtube.com/watch?v=y5vE8y_f_OM",
+			"With UrDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.",
+		href: "https://www.github.com/",
 		texture: "/textures/project/project2.mp4",
 		logo: "/assets/project-logo2.png",
 		logoStyle: {
@@ -103,22 +98,17 @@ export const myProjects = [
 			},
 			{
 				id: 3,
-				name: "TypeScript",
-				path: "/assets/typescript.png",
-			},
-			{
-				id: 4,
 				name: "Framer Motion",
 				path: "/assets/framer.png",
 			},
 		],
 	},
 	{
-		title: "CarePlus - Health Management System",
+		title: "MediFlow - Health Management System",
 		desc: "An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.",
 		subdesc:
-			"With a focus on efficiency, CarePlus integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.",
-		href: "https://www.youtube.com/watch?v=lEflo_sc82g",
+			"With a focus on efficiency, MediFlow integrantes complex forms and SMS notifications, by using React.js that enhance operational workflows.",
+		href: "https://www.github.com/",
 		texture: "/textures/project/project3.mp4",
 		logo: "/assets/project-logo3.png",
 		logoStyle: {
@@ -153,11 +143,11 @@ export const myProjects = [
 		],
 	},
 	{
-		title: "Horizon - Online Banking Platform",
-		desc: "Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.",
+		title: "Credable - Online Banking Platform",
+		desc: "Credable is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.",
 		subdesc:
-			"Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.",
-		href: "https://www.youtube.com/watch?v=PuOVqP_cjkE",
+			"Built with React.js, Credable ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.",
+		href: "https://www.github.com/",
 		texture: "/textures/project/project4.mp4",
 		logo: "/assets/project-logo4.png",
 		logoStyle: {
@@ -182,19 +172,14 @@ export const myProjects = [
 				name: "TypeScript",
 				path: "/assets/typescript.png",
 			},
-			{
-				id: 4,
-				name: "Framer Motion",
-				path: "/assets/framer.png",
-			},
 		],
 	},
 	{
-		title: "Imagica -  Photo Manipulation App",
-		desc: "Imagica is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using technology. With features like image editing, a payments system, and a credits-based model.",
+		title: "Incignia -  Photo Manipulation App",
+		desc: "Incignia is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using technology. With features like image editing, a payments system, and a credits-based model.",
 		subdesc:
-			"Built with Next.js 14, Cloudinary, Clerk, and Stripe, Imagica combines cutting-edge technology with a user-centric approach.",
-		href: "https://www.youtube.com/watch?v=Ahwoks_dawU",
+			"Built with React.js and Tailwind CSS, Incignia combines cutting-edge technology with a user-centric approach.",
+		href: "https://www.github.com/",
 		texture: "/textures/project/project5.mp4",
 		logo: "/assets/project-logo5.png",
 		logoStyle: {
@@ -218,11 +203,6 @@ export const myProjects = [
 				id: 3,
 				name: "TypeScript",
 				path: "/assets/typescript.png",
-			},
-			{
-				id: 4,
-				name: "Framer Motion",
-				path: "/assets/framer.png",
 			},
 		],
 	},
@@ -272,7 +252,6 @@ export const workExperiences = [
 		title:
 			"Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
 		icon: "/assets/framer.svg",
-		animation: "victory",
 	},
 	{
 		id: 2,
@@ -282,7 +261,6 @@ export const workExperiences = [
 		title:
 			"Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
 		icon: "/assets/figma.svg",
-		animation: "clapping",
 	},
 	{
 		id: 3,
@@ -292,7 +270,6 @@ export const workExperiences = [
 		title:
 			"Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
 		icon: "/assets/notion.svg",
-		animation: "salute",
 	},
 ];
 
